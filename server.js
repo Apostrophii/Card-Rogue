1cbfdb897cb1b5d369b5afc407bd816682113399
Apostrophii <luke.foobar@gmail.com>
2015-01-14 14:44:25 -0800

    At long last the client is actually connecting now. It works :)

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-var server = http.createServer(app).listen(8001);
-var io = require('socket.io').listen(server);
+//old way
+//var server = http.createServer(app).listen(8001);
+//var io = require('socket.io').listen(server);
 var logger = require('morgan');
 var favicon = require('serve-favicon');
 
@@ -22,8 +23,22 @@ app.use(function(req, res){
     res.send('404');
 });
 
-io.sockets.on('connection', function(socket){
+//new way
+var server = app.listen(8001);
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
     console.log('a user connected');
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data){
+        console.log(data);
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+    });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 
 //the base app model is at app.js
