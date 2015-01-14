fdd6dc5edaab9826e880e28ce32b59ab0b84a0db
Apostrophii <luke.foobar@gmail.com>
2015-01-14 15:23:02 -0800

    Non-local access works too now.

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-//old way
-//var server = http.createServer(app).listen(8001);
-//var io = require('socket.io').listen(server);
 var logger = require('morgan');
 var favicon = require('serve-favicon');
 
@@ -29,12 +26,9 @@ var io = require('socket.io').listen(server);
 
 io.on('connection', function(socket){
     console.log('a user connected');
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data){
-        console.log(data);
-    });
     socket.on('chat message', function(msg){
         console.log('message: ' + msg);
+        io.emit('chat message', msg);
     });
     socket.on('disconnect', function(){
         console.log('user disconnected');
