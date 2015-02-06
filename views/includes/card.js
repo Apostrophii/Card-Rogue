7cec2fc6a9227041bd826ddb4eb18d8a1cf4ce5c
Apostrophii <luke.foobar@gmail.com>
2015-02-06 11:53:19 -0800

    Card movement, rotation, and text position is fully functional.

--- views/includes/card.js
+++ views/includes/card.js
@@ -2,52 +2,68 @@ function Card(front, back, text, preset) {
     this.front = new createjs.Bitmap(front);
     this.back = new createjs.Bitmap(back);
 
-    if (preset == "standard") {
-        this.edgeOffset = 15 * RATIO;
-        this.yOffset = 0;
-        this.text = new createjs.Text(text, "15px Arial", "#777777");
+    this.cardBounds = this.front.getBounds();
+    
+    this.front.regX = this.cardBounds.width / 2;
+    this.front.regY = this.cardBounds.height / 2;
+    this.back.regX = this.cardBounds.width / 2;
+    this.back.regY = this.cardBounds.height / 2;
+
+    if (preset == "center") {
+        this.edgeOffset = 30 * RATIO;
+        this.text = new createjs.Text(text, "15px Arial", "#CC0000");
+        this.text.lineWidth = this.cardBounds.width - (this.edgeOffset * 2);
+        this.textHeight = this.text.getMeasuredHeight();
         this.text.textAlign = "center";
+        this.text.regY = this.textHeight / 2; //apparently only the regY is needed for text
+        this.faceUp = true;
+        this.back.scaleX = 0;
+    } else if (preset == "top") {
+        this.edgeOffset = 30 * RATIO;
+        this.text = new createjs.Text(text, "15px Arial", "#CC0000");
+        this.text.lineWidth = this.cardBounds.width - (this.edgeOffset * 2);
+        this.textHeight = this.text.getMeasuredHeight();
+        this.text.textAlign = "center";
+        this.text.regY = (this.cardBounds.height / 2) - this.edgeOffset; //apparently only the regY is needed for text
+        this.faceUp = true;
+        this.back.scaleX = 0;
+    } else if (preset == "bottom") {
+        this.edgeOffset = 30 * RATIO;
+        this.text = new createjs.Text(text, "15px Arial", "#CC0000");
+        this.text.lineWidth = this.cardBounds.width - (this.edgeOffset * 2);
+        this.textHeight = this.text.getMeasuredHeight();
+        this.text.textAlign = "center";
+        this.text.regY = -(this.cardBounds.height / 2) + this.edgeOffset + this.textHeight; //apparently only the regY is needed for text
         this.faceUp = true;
         this.back.scaleX = 0;
     } else {
         console.log("INVALID PRESET");
     }
 
-    this.bounds = this.front.getBounds();
-    this.text.lineWidth = this.bounds.width - (this.edgeOffset * 2);
-
-    this.front.regX = this.bounds.width / 2;
-    this.front.regY = this.bounds.height / 2;
-    this.back.regX = this.bounds.width / 2;
-    this.back.regY = this.bounds.height / 2;
-
     this.card = new createjs.Container();
     this.card.addChild(this.front, this.back, this.text);
     this.card.scaleX = RATIO;
     this.card.scaleY = RATIO;
-    this.card.x = 250; //default starting position
+    this.card.x = -250; //default starting position
     this.card.y = 250; 
     stage.addChild(this.card);
     stage.update(); //might not need this
 
-    this.move = function(x, y, time, delay) {
-        createjs.Tween.get(this.card).wait(delay, true).to({x: x, y: y}, time);
+    this.move = function(x1, y1, x2, y2, time, delay) {
+        createjs.Tween.get(this.card).wait(delay, true).to({x: x1, y: y1}, 0).to({x: x2, y: y2}, time);
     }
 
     this.flip = function(time, delay) {
         if (this.faceUp) {
             this.faceUp = false;
-            createjs.Tween.get(this.text).wait(delay, true).to({scaleX: 0}, time / 2);//flip text
-            createjs.Tween.get(this.front).wait(delay, true).to({scaleX: 0}, time / 2).call(function(back) { //flip front
-                createjs.Tween.get(back).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip back
-            }, [this.back]);
+            createjs.Tween.get(this.text).wait(delay, true).to({scaleX: 1}, 0).to({scaleX: 0}, time / 2); //flip text
+            createjs.Tween.get(this.front).wait(delay, true).to({scaleX: 1}, 0).to({scaleX: 0}, time / 2).play(this.flipBack); //flip front
+            createjs.Tween.get(this.back).wait(delay + time / 2, true).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip back
         } else {
             this.faceUp = true;
-            this.flipBackDown = createjs.Tween.get(this.back).wait(delay, true).to({scaleX: 0}, time / 2).call(function(text, front) { //flip back
-                createjs.Tween.get(text).call(function(front) {
-                    createjs.Tween.get(front).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip front
-                }, [front]).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip text
-            }, [this.text, this.front]); //ugly way to do things but it works
+            createjs.Tween.get(this.back).wait(delay, true).to({scaleX: 1}, 0).to({scaleX: 0}, time / 2).play(this.flipFront).play(this.flipText); //flip back
+            createjs.Tween.get(this.front).wait(delay + time / 2, true).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip front
+            createjs.Tween.get(this.text).wait(delay + time / 2, true).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip text
         }
     }
 }
