db799cc16d92793fc2ae554efa6b8a814d93ef82
Apostrophii <luke.foobar@gmail.com>
2015-02-17 16:07:50 -0800

    Rewrote card class one more time for increased functionality.

--- views/includes/card.js
+++ views/includes/card.js
@@ -1,14 +1,31 @@
-function Card(front, back, text, preset) {
-    this.front = new createjs.Bitmap(front);
-    this.back = new createjs.Bitmap(back);
+function Card(stage, front, back) {
+    this.frontFace = new createjs.Bitmap(front); //create bitmaps for front and back
+    this.backFace = new createjs.Bitmap(back);
 
-    this.cardBounds = this.front.getBounds();
-    
-    this.front.regX = this.cardBounds.width / 2;
-    this.front.regY = this.cardBounds.height / 2;
-    this.back.regX = this.cardBounds.width / 2;
-    this.back.regY = this.cardBounds.height / 2;
+    this.card = new createjs.Container(); //create card container
+    this.front = new createjs.Container(); //create front and back container
+    this.back = new createjs.Container();
+    this.front.addChild(this.frontFace); //add children to containers
+    this.back.addChild(this.backFace);
+    this.card.addChild(this.front, this.back);
 
+    this.cardBounds = this.frontFace.getBounds(); //set the local card origin for the front and back
+    this.frontFace.regX = this.cardBounds.width / 2;
+    this.frontFace.regY = this.cardBounds.height / 2;
+    this.backFace.regX = this.cardBounds.width / 2;
+    this.backFace.regY = this.cardBounds.height / 2;
+
+    this.faceUp = true; //start the card out fliped face up
+    this.back.scaleX = 0;
+
+    this.card.scaleX = RATIO; //set ratio
+    this.card.scaleY = RATIO;
+    this.card.x = -250; //default starting position
+    this.card.y = 250; 
+    stage.addChild(this.card); //add card to stage
+    stage.update(); //might not need this
+
+    /*
     if (preset == "center") {
         this.edgeOffset = 30 * RATIO;
         this.text = new createjs.Text(text, "15px Arial", "#CC0000");
@@ -17,7 +34,7 @@ function Card(front, back, text, preset) {
         this.text.textAlign = "center";
         this.text.regY = this.textHeight / 2; //apparently only the regY is needed for text
         this.faceUp = true;
-        this.back.scaleX = 0;
+        this.backFace.scaleX = 0;
     } else if (preset == "top") {
         this.edgeOffset = 30 * RATIO;
         this.text = new createjs.Text(text, "15px Arial", "#CC0000");
@@ -26,7 +43,7 @@ function Card(front, back, text, preset) {
         this.text.textAlign = "center";
         this.text.regY = (this.cardBounds.height / 2) - this.edgeOffset; //apparently only the regY is needed for text
         this.faceUp = true;
-        this.back.scaleX = 0;
+        this.backFace.scaleX = 0;
     } else if (preset == "bottom") {
         this.edgeOffset = 30 * RATIO;
         this.text = new createjs.Text(text, "15px Arial", "#CC0000");
@@ -35,19 +52,11 @@ function Card(front, back, text, preset) {
         this.text.textAlign = "center";
         this.text.regY = -(this.cardBounds.height / 2) + this.edgeOffset + this.textHeight; //apparently only the regY is needed for text
         this.faceUp = true;
-        this.back.scaleX = 0;
+        this.backFace.scaleX = 0;
     } else {
         console.log("INVALID PRESET");
     }
-
-    this.card = new createjs.Container();
-    this.card.addChild(this.front, this.back, this.text);
-    this.card.scaleX = RATIO;
-    this.card.scaleY = RATIO;
-    this.card.x = -250; //default starting position
-    this.card.y = 250; 
-    stage.addChild(this.card);
-    stage.update(); //might not need this
+    */
 
     this.move = function(x1, y1, x2, y2, time, delay) {
         createjs.Tween.get(this.card).wait(delay, true).to({x: x1, y: y1}, 0).to({x: x2, y: y2}, time);
@@ -56,14 +65,58 @@ function Card(front, back, text, preset) {
     this.flip = function(time, delay) {
         if (this.faceUp) {
             this.faceUp = false;
-            createjs.Tween.get(this.text).wait(delay, true).to({scaleX: 1}, 0).to({scaleX: 0}, time / 2); //flip text
-            createjs.Tween.get(this.front).wait(delay, true).to({scaleX: 1}, 0).to({scaleX: 0}, time / 2).play(this.flipBack); //flip front
+            createjs.Tween.get(this.front).wait(delay, true).to({scaleX: 1}, 0).to({scaleX: 0}, time / 2); //flip front
             createjs.Tween.get(this.back).wait(delay + time / 2, true).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip back
         } else {
             this.faceUp = true;
-            createjs.Tween.get(this.back).wait(delay, true).to({scaleX: 1}, 0).to({scaleX: 0}, time / 2).play(this.flipFront).play(this.flipText); //flip back
+            createjs.Tween.get(this.back).wait(delay, true).to({scaleX: 1}, 0).to({scaleX: 0}, time / 2); //flip back
             createjs.Tween.get(this.front).wait(delay + time / 2, true).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip front
-            createjs.Tween.get(this.text).wait(delay + time / 2, true).to({scaleX: 0}, 0).to({scaleX: 1}, time / 2); //flip text
+        }
+    }
+
+    this.updateElem = function(preset, data) {
+        if (this.front.getChildByName(preset)) {
+            this.front.getChildByName(preset).set(data);
+        } else if (this.back.getChildByName(preset)) {
+            this.back.getChildByName(preset).set(data);
+        } else {
+            console.log("PRESET DOES NOT EXIST");
+            return;
+        }
+    }
+
+    this.addElem = function(preset, data) {
+        if (this.front.getChildByName(preset)) { //get rid of this preset if it already exists
+            this.front.removeChild(this.front.getChildByName(preset));
+        } else if (this.back.getChildByName(preset)) {
+            this.back.removeChild(this.back.getChildByName(preset));
+        }
+        if (preset == "center") {
+            var edgeOffset = 30 * RATIO;
+            var child = new createjs.Text(data, "15px Arial", "#CC0000");
+            child.name = preset;
+            child.lineWidth = this.cardBounds.width - (edgeOffset * 2);
+            child.textAlign = "center";
+            child.regY = child.getMeasuredHeight() / 2;
+            this.front.addChild(child);
+        } else if (preset == "top") {
+            var edgeOffset = 30 * RATIO;
+            var child = new createjs.Text(data, "15px Arial", "#CC0000");
+            child.name = preset;
+            child.lineWidth = this.cardBounds.width - (edgeOffset * 2);
+            child.textAlign = "center";
+            child.regY = (this.cardBounds.height / 2) - edgeOffset;
+            this.front.addChild(child);
+        } else if (preset == "bottom") {
+            var edgeOffset = 30 * RATIO;
+            var child = new createjs.Text(data, "15px Arial", "#CC0000");
+            child.name = preset;
+            child.lineWidth = this.cardBounds.width - (edgeOffset * 2);
+            child.textAlign = "center";
+            child.regY = -(this.cardBounds.height / 2) + edgeOffset + child.getMeasuredHeight();
+            this.front.addChild(child);
+        } else {
+            console.log("INVALID PRESET");
         }
     }
 }
